adcbb1008c14bdaf3dc59fe002a789894f4943f2
Michael Dimitras <michaeladimitras@gmail.com>
2018-01-17 23:30:57 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  #for i in colors do
-  #  puts i.chars.first + " " + i
-  #end
-puts colors[0][0]
+  for i in colors do
+    puts i.chars.first.capitalize + " " + i
+  end
 end
